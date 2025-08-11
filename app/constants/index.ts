557d8d8a4aed6464d9ccf12e59ca1da6f418d477
Najmaldin ishag <najmaldinosman@gmail.com>
2025-08-11 22:59:55 +0300

    add constant/index.ts

---
+++ app/constants/index.ts
@@ -0,0 +1,26 @@
+export const Features = [
+  {
+    id: "1",
+    imgPath: "/assets/home/desktop/illustration-passionate.svg",
+    alt: "illustration",
+    title: "Passionate",
+    description:
+      "Each project starts with an in-depth brand research to ensure we only create products that serve a purpose. We merge art, design, and technology into exciting new solutions.",
+  },
+  {
+    id: "1",
+    imgPath: "/assets/home/desktop/illustration-resourceful.svg",
+    alt: "illustration",
+    title: "Resourceful",
+    description:
+      "Everything that we do has a strategic purpose. We use an agile approach in all of our projects and value customer collaboration. It guarantees superior results that fulfill our clients’ needs..",
+  },
+  {
+    id: "1",
+    imgPath: "/assets/home/desktop/illustration-friendly.svg",
+    alt: "illustration",
+    title: "Friendly",
+    description:
+      "We are a group of enthusiastic folks who know how to put people first. Our success depends on our customers, and we strive to give them the best experience a company can provide.",
+  },
+];
